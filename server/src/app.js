4b101376b8559e3cfda1b88d8afb1b638cc08de5
haidousm <haidous.m@gmail.com>
2021-11-17 15:22:48 +0200

    create post prods route

--- server/src/app.js
+++ server/src/app.js
@@ -3,10 +3,13 @@ const app = express();
 const cors = require("cors");
 const mongoose = require("mongoose");
 
+const mongoUri = `mongodb+srv://new-user_31:lana123@cluster0.x4n3c.mongodb.net/url_shortener?retryWrites=true&w=majority`;
+mongoose.connect(mongoUri, { useNewUrlParser: true, useUnifiedTopology: true });
+
 app.use(cors());
 app.use(express.json());
 
-app.get("/products", require("./routes/products"));
+app.use("/products", require("./routes/products"));
 
 const port = process.env.PORT || 5100;
 app.listen(port, () => console.log(`Listening on port ${port}`));
--- server/src/routes/products.js
+++ server/src/routes/products.js
@@ -4,10 +4,26 @@ const Product = require("../models/product");
 
 // @desc Get all products
 // @route GET /products/
-router.get("/products", async (req, res) => {
-    // get all products from mongoDB
+router.get("/", async (req, res) => {
     const products = await Product.find();
     res.json(products);
+});
+
+// @desc Create a product
+// @route POST /products/
+router.post("/", async (req, res) => {
+    const product = new Product({
+        name: req.body.name,
+        price: req.body.price,
+        description: req.body.description,
+        image: req.body.image,
+        quantity: req.body.quantity,
+        brand: req.body.brand,
+    });
+    await product.save();
+    res.json({
+        message: "Product created successfully",
+    });
 });
 
 module.exports = router;
