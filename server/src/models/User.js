07ced1f4001fdde0a6b27287cb6176972ebeac6c
haidousm <haidous.m@gmail.com>
2021-12-02 19:24:42 +0200

    setup server + some models

---
+++ server/src/models/User.js
@@ -0,0 +1,41 @@
+const mongoose = require("mongoose");
+const UserSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    hash: {
+        type: String,
+        required: true,
+    },
+    salt: {
+        type: String,
+        required: true,
+    },
+    firstName: {
+        type: String,
+        required: true,
+    },
+    lastName: {
+        type: String,
+        required: true,
+    },
+
+    addresses: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Address",
+        },
+    ],
+    createdAt: {
+        type: Date,
+        default: Date.now,
+    },
+});
+
+UserSchema.set("toJSON", {
+    virtuals: true,
+});
+
+module.exports = mongoose.model("User", UserSchema);
