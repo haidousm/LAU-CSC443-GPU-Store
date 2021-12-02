5f22b84f7317d7f37f76983bcb5ba782fc00f27c
haidousm <haidous.m@gmail.com>
2021-12-02 20:37:21 +0200

    get products by brand route

--- server/src/routes/products.js
+++ server/src/routes/products.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 const Product = require("../models/Product");
+const Brand = require("../models/Brand");
 
 /**
  * @route GET /products
@@ -10,7 +11,7 @@ const Product = require("../models/Product");
  * @access Public
  */
 router.get("/", async (req, res) => {
-    const { num, search, sort } = req.query;
+    const { num, search, sort, brand } = req.query;
     const limit = num ? parseInt(num) : 10;
     const searchTerm = search ? search : "";
     const query = searchTerm
@@ -19,6 +20,31 @@ router.get("/", async (req, res) => {
     const sortBy = sort ? sort : "asc";
 
     const products = await Product.find(query)
+        .limit(limit)
+        .sort({ price: sortBy });
+    res.send(products);
+});
+
+/**
+ * @route GET /products/:brand
+ * @desc Get all products by brand
+ * @param num - number of products to return (default: 10) | search - search term | sort - sort by price (asc/desc)
+ * @access Public
+ */
+router.get("/:brand", async (req, res) => {
+    const { num, search, sort } = req.query;
+    const limit = num ? parseInt(num) : 10;
+    const searchTerm = search ? search : "";
+    const query = searchTerm
+        ? { name: { $regex: searchTerm, $options: "i" } }
+        : {};
+    const sortBy = sort ? sort : "asc";
+    const brand = req.params.brand;
+
+    const brandObj = await Brand.findOne({ name: brand });
+    if (!brandObj) return res.status(404).send("Brand not found");
+
+    const products = await Product.find({ brand: brandObj._id, ...query })
         .limit(limit)
         .sort({ price: sortBy });
     res.send(products);
